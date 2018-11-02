d89ad58000da4406131b4a6801dd36900e8ecb19
DINI PUSPITA SUKMA ARIYANTI <i16084@ftis>
2018-11-02 14:30:30 +0700

    tambah kurang

--- JavaApplication2/src/Calculator.java
+++ JavaApplication2/src/Calculator.java
@@ -14,11 +14,11 @@ public abstract class Calculator {
     }
     
     public float plus(float a, float b) {
-        
+        return a + b ;
     }
     
     public float minus(float a, float b) {
-        
+        return a-b ;
     }
     
     public float multiply(float a, float b) {
